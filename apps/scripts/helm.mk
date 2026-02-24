3bcea5dd5bce886598a00894dd6c8afe1a4bbef5
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2026-02-24 14:15:14 +0200

    Deployment up and running, missing connectivity tests

--- apps/scripts/helm.mk
+++ apps/scripts/helm.mk
@@ -3,7 +3,7 @@ APP?="demo"
 install:
 	helm install ${APP} . -f values.yaml -f base.values.yaml -n ${APP} --create-namespace
 
-update:
+upgrade:
 	helm upgrade ${APP} . -f values.yaml -f base.values.yaml -n ${APP} --create-namespace
 
 template:
